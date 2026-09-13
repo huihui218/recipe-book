0805ce5b1851ea872bde336ae2a8987c35af9982
huihui218 <328628171+huihui218@users.noreply.github.com>
2026-09-13 20:20:58 +0800

    离线缓存升级v2+页面网络优先：手机端更新不再被旧缓存卡住

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 我的菜谱 Service Worker：离线可用 */
-const CACHE='caipu-v1';
+/* 我的菜谱 Service Worker：离线可用；页面本身网络优先（更新能尽快到手机），断网时退回缓存 */
+const CACHE='caipu-v2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
@@ -9,6 +9,14 @@ self.addEventListener('activate',e=>{
 });
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'){ /* 打开页面先联网拿最新，拿不到再给缓存的 */
+    e.respondWith(fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>caches.match('./index.html')));
+    return;
+  }
   e.respondWith(
     caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
       const copy=res.clone();
